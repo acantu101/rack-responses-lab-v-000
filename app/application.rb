8a0e5e4c38bbb42c895c88562edab46ad1c5689d
alejandra cantu <alejandra.cantu13@gmail.com>
2021-01-07 01:50:28 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,23 +1,19 @@
-require 'pry'
-#class Application
 
-  #def call(env)
-#     resp = Rack::Response.new
-binding.pry
+class Application
+
+def call(env)
+   resp = Rack::Response.new
+   time = Time.now.hour
 
 
-#end
-#     num_1 = Kernel.rand(1..20)
-#     num_2 = Kernel.rand(1..20)
-#     num_3 = Kernel.rand(1..20)
-#
-#     if num_1==num_2 && num_2==num_3
-#       resp.write "You Win"
-#     else
-#       resp.write "You Lose"
-#     end
-#
-#     resp.finish
-#   end
-#
- #end
+
+     if time <= 12
+       resp.write "Good Morning!"
+     else
+      resp.write "Good Afternoon"
+    end
+
+    resp.finish
+  end
+
+end
